be3a4070b7f01799ed8aa1bda28ebfe56149d86b
tmorgane <morgane.tschaen@ynov.com>
2026-09-15 17:17:02 +0200

    Add library page

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,7 +1,22 @@
 function Library() {
   return (
     <main>
-      <h1>Bibliothèque</h1>
+      <h1>Ma bibliothèque</h1>
+
+      <section>
+        <h2>À regarder</h2>
+        <p>Aucun film dans cette liste.</p>
+      </section>
+
+      <section>
+        <h2>En cours</h2>
+        <p>Aucun film dans cette liste.</p>
+      </section>
+
+      <section>
+        <h2>Vu</h2>
+        <p>Aucun film dans cette liste.</p>
+      </section>
     </main>
   )
 }
