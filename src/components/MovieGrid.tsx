f78542e8887e35166526f31d3e8faf8a2953c93d
tmorgane <morgane.tschaen@ynov.com>
2026-09-17 15:02:51 +0200

    Use TMDB movies on home

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,40 +1,20 @@
 import MovieCard from "./MovieCard"
-import { movies } from "../data/movies"
 import type { Movie } from "../types/Movie"
 
 type MovieGridProps = {
+  movies: Movie[]
   favorites: Movie[]
   onToggleFavorite: (movie: Movie) => void
-  search: string
-  onResetSearch: () => void
 }
 
 function MovieGrid({
+  movies,
   favorites,
   onToggleFavorite,
-  search,
-  onResetSearch,
 }: MovieGridProps) {
-  const filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(search.toLowerCase())
-  )
-
-  if (filteredMovies.length === 0) {
-    return (
-      <section>
-        <p>Aucun film trouvé</p>
-        <p>Essayez avec un autre titre ou une autre recherche.</p>
-
-        <button onClick={onResetSearch}>
-          Réinitialiser la recherche
-        </button>
-      </section>
-    )
-  }
-
   return (
     <section>
-      {filteredMovies.map((movie) => (
+      {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           movie={movie}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import MovieGrid from "../components/MovieGrid"
 import { useAppContext } from "../context/AppContext"
+import useMovies from "../hooks/useMovies"
 
 function Home() {
   const { favorites, toggleFavorite } = useAppContext()
+
+  const {
+    movies,
+    loading,
+    error,
+  } = useMovies(1)
 
   return (
     <main>
@@ -15,12 +22,21 @@ function Home() {
       <section>
         <h2>Films populaires</h2>
 
-        <MovieGrid
-          favorites={favorites}
-          onToggleFavorite={toggleFavorite}
-          search=""
-          onResetSearch={() => {}}
-        />
+        {loading && (
+          <p>Chargement des films...</p>
+        )}
+
+        {error && (
+          <p>Impossible de charger les films.</p>
+        )}
+
+        {!loading && !error && (
+          <MovieGrid
+            movies={movies}
+            favorites={favorites}
+            onToggleFavorite={toggleFavorite}
+          />
+        )}
       </section>
     </main>
   )
